5d226beac9ac91970a05f5e7adb0d298e66126ef
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-01-28 12:49:04 +1100

    Implement pug

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 
-// app.get("/", (req,res) => {
-//     res.sendFile("./public/index.html")
-// });
-
-// app.get("/admin", (req,res) => {
-//     res.send("<h1>Hi this is the admin page</h1>");
-// });
+app.get("/", (req,res) => {
+    res.render("index", { title: "Tools for Fools"})
+});
 
 app.listen(3000, () => {
     console.log("Web server is running at port 3000");
