dbd77750dd6acef58f9c409caf7e86f831e28d59
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-01-28 11:37:36 +1100

    Statically served files

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("<p>Hello</p>");
-});
+app.use(express.static(__dirname + "/public"));
 
-app.get("/admin", (req,res) => {
-    res.send("<h1>Hi this is the admin page</h1>");
-});
+// app.get("/", (req,res) => {
+//     res.sendFile("./public/index.html")
+// });
+
+// app.get("/admin", (req,res) => {
+//     res.send("<h1>Hi this is the admin page</h1>");
+// });
 
 app.listen(3000, () => {
     console.log("Web server is running at port 3000");
