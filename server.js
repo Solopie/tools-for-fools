fe883ed7ed7d84eb364001bad639751d8ad3ebfc
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-01-28 13:08:39 +1100

    Implemented env variables

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+const config = require("./utils/config")
 const express = require("express");
 const app = express();
 
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/public"));
 
+let count = 0;
+
 app.get("/", (req,res) => {
-    res.render("index", { title: "Tools for Fools"})
+    count++;
+    res.render("index", { title: "Tools for Fools", count })
 });
 
-app.listen(3000, () => {
-    console.log("Web server is running at port 3000");
+app.listen(config.PORT, () => {
+    console.log(`Web server is running at port ${config.PORT}`);
 });
